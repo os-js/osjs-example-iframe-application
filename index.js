27adb81aa9a49311c9ac641cc0f6f7ae09a6a681
Anders Evenrud <andersevenrud@gmail.com>
2019-02-18 01:42:11 +0100

    Simplified example

--- index.js
+++ index.js
@@ -30,68 +30,6 @@
 import osjs from 'osjs';
 import {name as applicationName} from './metadata.json';
 
-const createSimpleIframe = (core, proc) => ($content, win) => {
-  const iframe = document.createElement('iframe');
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
-  iframe.setAttribute('border', '0');
-
-  const src = proc.resource('/data/index.html');
-  iframe.src = src;
-
-  $content.appendChild(iframe);
-};
-
-const createIframe = (core, proc) => ($content, win) => {
-
-  // Create a new bus for our messaging
-  const bus = core.make('osjs/event-handler', 'MyIframeApplicationWindow');
-
-  // Get path to iframe content
-  const src = proc.resource('/data/index.html');
-
-  // Create DOM element
-  const iframe = document.createElement('iframe');
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
-  iframe.setAttribute('border', '0');
-
-  iframe.addEventListener('load', () => {
-    const ref = iframe.contentWindow;
-
-    // This will proxy the window focus events to iframe
-    win.on('focus', () => ref.focus());
-    win.on('blur', () => ref.blur());
-
-    // Create message sending wrapper
-    const sendMessage = msg => ref.postMessage(msg, window.location.href);
-
-    // After connection is established, this handler will process
-    // all events coming from iframe.
-    proc.on('message', data => {
-      console.warn('[Application', 'Iframe sent', data);
-      bus.emit(data.method, sendMessage, ...data.args);
-    });
-
-    bus.on('yo', (send, args) => sendMessage({
-      method: 'yo',
-      args: ['MyIframeApplication says hello']
-    }));
-
-    // Send the process ID to our iframe to establish communication
-    sendMessage({
-      method: 'init',
-      args: [proc.pid]
-    });
-  });
-
-  // Finally set the source and attach
-  iframe.src = src;
-
-  // Attach
-  $content.appendChild(iframe);
-};
-
 // Creates the internal callback function when OS.js launches an application
 // Note the first argument is the 'name' taken from your metadata.json file
 const register = (core, args, options, metadata) => {
@@ -111,12 +49,14 @@ const register = (core, args, options, metadata) => {
     position: {left: 700, top: 200}
   })
     .on('destroy', () => proc.destroy())
-
-    // Create an iframe with messaging etc
-    .render(createIframe(core, proc));
-
-  // Or just a simple standard iframe
-  // .render(createIframe(core, proc));
+    .render($content => {
+      const iframe = document.createElement('iframe');
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.src = proc.resource('/data/index.html');
+      iframe.setAttribute('border', '0');
+      $content.appendChild(iframe);
+    });
 
   return proc;
 };
